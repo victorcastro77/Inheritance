d4847dd2568e24ac9213b07c42d8b79da12f6e0d
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-12-08 11:44:41 -0800

    inheritance

--- Inheritance/People.java
+++ Inheritance/People.java
@@ -1,23 +1,38 @@
 public class People {
     private int age;
+    private int health;
     private int strength;
-    String name;
+    private String theirname;
     
     public People() {
         age = 0;
+        health = 100;
         strength = 0;
     }
+    public String name(String name) {
+        theirname = name;
+        return "your name is: " +name;
+    }
+    public String getName() {
+        return theirname;
+    }
     public void eat() {
-        strength += 10;
+        health += 10;
     }
     public int getAge(){
         return age;
     }
     public void ageTenYears() {
         age += 10;
+        health -= 10;
     }
     public void workout() {
         strength += 20;
-        
     }
+    
+}
+public class Kid extends People {
+      public Kid() {
+          health = 100;
+      }
 }
